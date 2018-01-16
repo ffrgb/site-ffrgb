90b4b64e4bde0f9ff7aba913dd8f3c3fb038079d
Bastian <mephisto@mephis.to>
2018-01-16 17:51:41 +0100

    Include domain-select package

--- site.mk
+++ site.mk
@@ -6,6 +6,7 @@ GLUON_SITE_PACKAGES := \
 	gluon-config-mode-autoupdater \
 	gluon-config-mode-hostname \
 	gluon-config-mode-mesh-vpn \
+	gluon-config-mode-domain-select \
 	gluon-config-mode-geo-location \
 	gluon-config-mode-contact-info \
 	gluon-ebtables-filter-multicast \
